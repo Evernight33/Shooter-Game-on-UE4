f86ad2e06e6e5fd20bc05b6da666208fb620f0e6
Evernight33 <il.radkev@gmail.com>
2022-09-25 18:50:56 +0300

    Sound: Widget's openning sound

--- Source/ShootThemUp/Private/Menu/UI/STUMenuWidget.cpp
+++ Source/ShootThemUp/Private/Menu/UI/STUMenuWidget.cpp
@@ -8,6 +8,8 @@
 #include "Kismet/KismetSystemLibrary.h"
 #include "Components/HorizontalBox.h"
 #include "Menu/UI/STULevelItemWidget.h"
+#include "Sound/SoundCue.h"
+
 
 DEFINE_LOG_CATEGORY_STATIC(LogSTUMenuWidget, All, All)
 
@@ -44,6 +46,7 @@ void USTUMenuWidget::OnAnimationFinished_Implementation(const UWidgetAnimation* 
 void USTUMenuWidget::OnStartGame() 
 {
     PlayAnimation(HideAnimation);
+    UGameplayStatics::PlaySound2D(GetWorld(), StartGameSound);
 }
 
 void USTUMenuWidget::OnQuitGame()
